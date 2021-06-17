615f49511b52fb52e974b6a79a302e30bd3d2fb7
Shibobrota <shibobrota@gmail.com>
2021-06-17 23:30:22 +0530

    Popup working

--- projects/magic-library/src/lib/magic-library.module.ts
+++ projects/magic-library/src/lib/magic-library.module.ts
@@ -3,8 +3,10 @@ import { MagicLibraryComponent } from './magic-library.component';
 import { HttpClientModule } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 import { MatDialogModule } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
 import { PopupComponent } from './popup/popup.component';
 import { MatGridListModule } from '@angular/material/grid-list';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -15,10 +17,13 @@ import { MatGridListModule } from '@angular/material/grid-list';
     HttpClientModule,
     CommonModule,
     MatDialogModule,
-    MatGridListModule
+    MatGridListModule,
+    BrowserAnimationsModule,
+    MatButtonModule
   ],
   exports: [
     MagicLibraryComponent
-  ]
+  ],
+  entryComponents: [PopupComponent]
 })
 export class MagicLibraryModule { }
